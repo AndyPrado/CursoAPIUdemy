720761fc7a06533652cbaf491de55b7d9928a390
Anderson Prado <prado.app.dev@gmail.com>
2019-06-05 18:55:52 -0300

    Parte1 Conexão com o banco

---
+++ cursoAPI/cursoAPI/Models/Context/MySQLDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace cursoAPI.Models.Context
+{
+    public class MySQLDbContext : DbContext
+    {
+        public MySQLDbContext()
+        {
+
+        }
+
+        public MySQLDbContext(DbContextOptions<MySQLDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Person> Persons { get; set; }
+    }
+}
