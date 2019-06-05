68e4af9ce9f379d8d175669f6a90916f0ef9b2cd
Anderson Prado <prado.app.dev@gmail.com>
2019-06-05 14:13:16 -0300

    Criação da interface de pessoas

---
+++ cursoAPI/cursoAPI/Services/IPersonService.cs
@@ -0,0 +1,14 @@
+﻿using cursoAPI.Models;
+using System.Collections.Generic;
+
+namespace cursoAPI.Services
+{
+    interface IPersonService
+    {
+        Person Create(Person person);
+        Person FindById(int personId);
+        List<Person> FindAll();
+        Person Update(Person person);
+        void Delete(int id);
+    }
+}
